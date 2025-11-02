577400ad31be5c1e629ae8f1796a7fa723d2ea4d
runningpoem30 <aryaanandpathak30@gmail.com>
2025-11-03 00:43:20 +0530

    g

---
+++ personalPortfolio/src/App.tsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+
+function MyComponent() 
+  const myStyle = {
+    backgroundColor: 'black', // CSS property 'background-color' becomes 'backgroundColor' in React
+    color: 'white',
+    padding: '10px',
+    borderRadius: '5px'
+  };
+
+
+function App() {
+
+  return (
+   <div style={myStyle}>
+    <p>revamping my portfolio website</p>
+    <p>coming soon , stay tuned! </p>
+    <p>septarya30@gmail.com</p>
+    <p>X - @PoemRunning</p>
+   </div>
+
+  )
+}
+
+export default App
